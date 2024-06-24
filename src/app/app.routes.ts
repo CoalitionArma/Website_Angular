ceef3d97c078ddf813dcc03ea11d8c0f16f3c187
Troy <tanaka_s2@yahoo.com>
2024-06-23 23:56:04 -0500

    Basic header + css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StatsComponent } from './stats/stats.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent,
+        data: {
+            title: 'Home'
+        },
+    },
+    {
+        path: 'stats', component: StatsComponent,
+        data: {
+            title: 'Stats'
+        },
+    },
+];
