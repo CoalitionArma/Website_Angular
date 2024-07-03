2f277541b168a611177d41b75a81cb6595b38c3e
Davis Bray <pdavisbray@gmail.com>
2024-07-03 22:07:41 +0300

    Finished adding wide screen support

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
       "./src/**/*.{html,ts}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        screens: {
+          wide: "2000px"
+        }
+      }
     },
     plugins: [],
   }
