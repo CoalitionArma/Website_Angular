ab9a655ddb84f813a1b36aae2720a9187cd7f8fe
Davis Bray <pdavisbray@gmail.com>
2024-07-04 09:13:22 -0500

    started conversion to coreui header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { DropdownModule } from '@coreui/angular';
+import { DropdownModule, GridModule, NavModule, HeaderModule} from '@coreui/angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,RouterOutlet,DropdownModule,],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet,DropdownModule, HeaderModule, GridModule, NavModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
