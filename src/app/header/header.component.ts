3bfdb756c4d61c9a7dfb39ce8e8ed36737389830
GHPCee <greggithub2001@gmail.com>
2024-07-03 22:10:03 -0400

    Introduce basic dropdown

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DropdownModule } from '@coreui/angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,RouterOutlet],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet,DropdownModule,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
