43c1a2d4b463a5cb3e3f25a2289bdb6eae0cf5b0
GHPCee <greggithub2001@gmail.com>
2024-07-04 12:02:24 -0400

    Start angular/material header buttons and menus

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DropdownModule, GridModule, NavModule, HeaderModule} from '@coreui/angular';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,RouterOutlet,DropdownModule, HeaderModule, GridModule, NavModule],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet,DropdownModule, HeaderModule, GridModule, NavModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
