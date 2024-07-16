fd3b3f102dad94bab8f3d346a896e23c6534eee2
Jesse van Gool <jvgool2001@gmail.com>
2024-07-16 15:09:30 +0200

    cfc page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,22 @@ import { StatsComponent } from './stats/stats.component';
 import { CfcComponent } from './cfc/cfc.component';
 
 export const routes: Routes = [
-    {
-        path: '', component: HomeComponent,
-        data: {
-            title: 'Home'
-        },
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      title: 'Home',
     },
-    {
-        path: 'stats', component: StatsComponent,
-        data: {
-            title: 'Stats'
-        },
+  },
+  {
+    path: 'stats',
+    component: StatsComponent,
+    data: {
+      title: 'Stats',
     },
-    {
-        path: '', component: CfcComponent,
-        data: {
-            title: 'CFC'
-        },
-    },
+  },
+  {
+    path: 'cfc',
+    component: CfcComponent,
+  },
 ];
