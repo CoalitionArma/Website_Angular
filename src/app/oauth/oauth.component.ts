eb25c2f0ac437df75dac0f2f357e64ba1e56312f
Troy <tanaka_s2@yahoo.com>
2024-12-20 16:37:18 -0600

    Remove unused response

--- src/app/oauth/oauth.component.ts
+++ src/app/oauth/oauth.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
-import { DBUserResponse } from '../interfaces/user.interface';
 
 @Component({
   selector: 'app-oauth',
