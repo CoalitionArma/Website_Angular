47be01b869e86951d90dfc16c519648fd1650ef1
Troy <tanaka_s2@yahoo.com>
2024-12-21 15:04:28 -0600

    Api stuff

--- backend/app.ts
+++ backend/app.ts
@@ -14,6 +14,12 @@ const app = express();
 const port = 3157;
 const allowedOrigins = ['http://localhost:4200', 'http://coalitiongroup.net', 'https://coalitiongroup.net'];
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 // TODO: Move elsewhere
 const JWT_SECRET = process.env['JWT_SECRET'] as string;
 function generateToken(userId: string): string {
