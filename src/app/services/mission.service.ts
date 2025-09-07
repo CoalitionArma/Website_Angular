9baa6ad2ab3f44a25d758a72dc18314750df87f4
Troy <tanaka_s2@yahoo.com>
2025-09-07 15:28:13 -0500

    Fix api url

--- src/app/services/mission.service.ts
+++ src/app/services/mission.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Mission, MissionsResponse, MissionFilters } from '../interfaces/mission.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MissionService {
-  private apiUrl = 'http://localhost:3157/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
--- src/app/services/replay.service.ts
+++ src/app/services/replay.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ReplayFile, ReplaysResponse } from '../interfaces/replay.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReplayService {
-  private apiUrl = 'http://localhost:3157/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
