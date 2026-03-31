8181673a7522b71d6dc0f86b1d4b51112b9440b3
Troy <tanaka_s2@yahoo.com>
2026-03-30 23:12:53 -0500

    Event enviro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.coalitiongroup.net/api',
-  botApiUrl: 'http://163.245.195.90:5000',
+  botApiUrl: 'http://localhost:5000',
   discordApiUrl: 'https://discord.com/api',
   discordCdnUrl: 'https://cdn.discordapp.com',
   clientId: '1311851378875830354',
