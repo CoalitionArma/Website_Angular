2ffeee396d424aed0dd8b27c0b198c527f1f09f9
Troy <tanaka_s2@yahoo.com>
2026-04-01 19:21:32 -0500

    Behold. Security.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.coalitiongroup.net/api',
-  botApiUrl: 'http://bot.coalitiongroup.net',
+  botApiUrl: 'https://bot.coalitiongroup.net',
   discordApiUrl: 'https://discord.com/api',
   discordCdnUrl: 'https://cdn.discordapp.com',
   clientId: '1311851378875830354',
